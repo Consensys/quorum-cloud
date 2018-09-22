3be5063682f5d6aae9fa1984103e51c208c2dccb
Trung Nguyen <trung.n.k@gmail.com>
2018-09-21 23:10:08 -0400

    reformat output for easy integration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/trung/quorum-acceptance-test.svg?branch=master)](https://travis-ci.org/trung/quorum-acceptance-test)
+
 Using [Terraform](https://terraform.io) to bootstrap Quorum network in the cloud.
 
 ### Prerequisites
--- aws/templates/outputs.tf
+++ aws/templates/outputs.tf
@@ -1,3 +1,23 @@
+output "_status" {
+  value = <<MSG
+Completed!
+
+Quorum Docker Image         = ${local.quorum_docker_image}
+Privacy Engine Docker Image = ${local.tx_privacy_engine_docker_image}
+Number of Quorum Nodes      = ${var.number_of_nodes}
+ECS Task Revision           = ${aws_ecs_task_definition.quorum.revision}
+CloudWatch Log Group        = ${aws_cloudwatch_log_group.quorum.name}
+MSG
+}
+
+output "bastion_host_dns" {
+  value = "${aws_instance.bastion.public_dns}"
+}
+
+output "bastion_host_ip" {
+  value = "${aws_instance.bastion.public_ip}"
+}
+
 output "network_name" {
   value = "${var.network_name}"
 }
@@ -16,21 +36,4 @@ output "private_key_file" {
 
 output "bucket_name" {
   value = "${aws_s3_bucket.quorum.bucket}"
-}
-
-output "status" {
-  value = <<MSG
-Completed!
-
-Quorum Docker Image         = ${local.quorum_docker_image}
-Privacy Engine Docker Image = ${local.tx_privacy_engine_docker_image}
-Quorum Network Name         = ${var.network_name}
-Number of Quorum Nodes      = ${var.number_of_nodes}
-ECS Cluster                 = ${aws_ecs_cluster.quorum.name}
-ECS Task Revision           = ${aws_ecs_task_definition.quorum.revision}
-CloudWatch Log Group        = ${aws_cloudwatch_log_group.quorum.name}
-Bastion Private Key File    = ${local_file.private_key.filename}
-Bastion Host Public IP      = ${aws_instance.bastion.public_ip}
-Bastion Host Public DNS     = ${aws_instance.bastion.public_dns}
-MSG
 }
