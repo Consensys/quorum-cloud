55427192cee3fedb73d229aed44a5ae8dc41c6d7
Trung Nguyen <trung.n.k@gmail.com>
2018-09-24 15:44:40 -0400

    docs

--- aws/templates/_terraform_init/outputs.tf
+++ aws/templates/_terraform_init/outputs.tf
@@ -8,6 +8,6 @@ Deployment ID: ${local.deployment_id}
 Generated ... ${local.tfinit_filename}
 Generated ... ${local.tfvars_filename}
 
-Now you can do `terraform init -backend-config=${local.tfinit_filename}` from ${dirname(local_file.tfinit.filename)}
+Now you can do `terraform init -backend-config=${local.tfinit_filename} -reconfigure` from ${dirname(local_file.tfinit.filename)}
 msg
 }
