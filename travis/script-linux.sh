7fc16a9efe9ce133fa088309b89e8b9ac9338851
amalraj.manigmail.com <amalraj.manigmail.com>
2019-02-12 23:02:59 +0800

    update start scripts

--- travis/script-linux.sh
+++ travis/script-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is called from TRAVIS CI configured in quorum repo. Its called from the script: part of the build job
-# This script brings up the 4node network for a specific consensus and runs the automated acceptance test against it
+# This script brings up the 4node network for a specific consensus
 
 consensus=${TF_VAR_consensus_mechanism}
 
@@ -12,11 +12,4 @@ cd $TRAVIS_HOME/quorum-cloud/travis/4nodes
 ./init.sh $consensus
 set -e
 ./start.sh $consensus tessera
-set -e
-cd $TRAVIS_HOME/quorum-acceptance-tests
-cp config/application-local.4nodes.yml config/application-local.yml
-echo "running acceptance test for consensus $consensus ..."
-./src/travis/run_tests.sh
-echo "stop the network..."
-$TRAVIS_HOME/quorum-cloud/travis/4nodes/stop.sh
 echo "script done"
