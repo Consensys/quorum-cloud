4b8e97efcac9df08db0be994b4350974d5ace4c5
Trung Nguyen <trung.n.k@gmail.com>
2019-06-24 12:53:08 +0100

    security group vpc from subnet

--- aws/templates/security_groups.tf
+++ aws/templates/security_groups.tf
@@ -1,13 +1,21 @@
+data "aws_subnet" "vpc" {
+  id = "${var.bastion_public_subnet_id}"
+}
+
 resource "aws_security_group" "quorum" {
-  name = "quorum-sg-${var.network_name}"
+  name        = "quorum-sg-${var.network_name}"
   description = "Security group used in Quorum network ${var.network_name}"
+  vpc_id      = "${data.aws_subnet.vpc.vpc_id}"
 
   egress {
     from_port = 0
-    protocol = "-1"
-    to_port = 0
+    protocol  = "-1"
+    to_port   = 0
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
+
     description = "Allow all"
   }
 
@@ -15,94 +23,97 @@ resource "aws_security_group" "quorum" {
 }
 
 resource "aws_security_group_rule" "ethstats" {
-  from_port = "${local.ethstats_port}"
-  protocol = "tcp"
+  from_port         = "${local.ethstats_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.ethstats_port}"
-  type = "ingress"
-  self = true
-  description = "ethstats traffic"
+  to_port           = "${local.ethstats_port}"
+  type              = "ingress"
+  self              = true
+  description       = "ethstats traffic"
 }
 
 resource "aws_security_group_rule" "geth_p2p" {
-  from_port = "${local.quorum_p2p_port}"
-  protocol = "tcp"
+  from_port         = "${local.quorum_p2p_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.quorum_p2p_port}"
-  type = "ingress"
-  self = true
-  description = "Geth P2P traffic"
+  to_port           = "${local.quorum_p2p_port}"
+  type              = "ingress"
+  self              = true
+  description       = "Geth P2P traffic"
 }
 
 resource "aws_security_group_rule" "geth_admin_rpc" {
-  from_port = "${local.quorum_rpc_port}"
-  protocol = "tcp"
+  from_port         = "${local.quorum_rpc_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.quorum_rpc_port}"
-  type = "ingress"
-  self = "true"
-  description = "Geth Admin RPC traffic"
+  to_port           = "${local.quorum_rpc_port}"
+  type              = "ingress"
+  self              = "true"
+  description       = "Geth Admin RPC traffic"
 }
 
 resource "aws_security_group_rule" "constellation" {
-  count = "${var.tx_privacy_engine == "constellation" ? 1 : 0}"
-  from_port = "${local.constellation_port}"
-  protocol = "tcp"
+  count             = "${var.tx_privacy_engine == "constellation" ? 1 : 0}"
+  from_port         = "${local.constellation_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.constellation_port}"
-  type = "ingress"
-  self = "true"
-  description = "Constellation API traffic"
+  to_port           = "${local.constellation_port}"
+  type              = "ingress"
+  self              = "true"
+  description       = "Constellation API traffic"
 }
 
 resource "aws_security_group_rule" "tessera" {
-  count = "${var.tx_privacy_engine == "tessera" ? 1 : 0}"
-  from_port = "${local.tessera_port}"
-  protocol = "tcp"
+  count             = "${var.tx_privacy_engine == "tessera" ? 1 : 0}"
+  from_port         = "${local.tessera_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.tessera_port}"
-  type = "ingress"
-  self = "true"
-  description = "Tessera API traffic"
+  to_port           = "${local.tessera_port}"
+  type              = "ingress"
+  self              = "true"
+  description       = "Tessera API traffic"
 }
 
 resource "aws_security_group_rule" "tessera_thirdparty" {
-  count = "${var.tx_privacy_engine == "tessera" ? 1 : 0}"
-  from_port = "${local.tessera_thirdparty_port}"
-  protocol = "tcp"
+  count             = "${var.tx_privacy_engine == "tessera" ? 1 : 0}"
+  from_port         = "${local.tessera_thirdparty_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.tessera_thirdparty_port}"
-  type = "ingress"
-  self = "true"
-  description = "Tessera Thirdparty API traffic"
+  to_port           = "${local.tessera_thirdparty_port}"
+  type              = "ingress"
+  self              = "true"
+  description       = "Tessera Thirdparty API traffic"
 }
 
 resource "aws_security_group_rule" "raft" {
-  count = "${var.consensus_mechanism == "raft" ? 1 : 0}"
-  from_port = "${local.raft_port}"
-  protocol = "tcp"
+  count             = "${var.consensus_mechanism == "raft" ? 1 : 0}"
+  from_port         = "${local.raft_port}"
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.quorum.id}"
-  to_port = "${local.raft_port}"
-  type = "ingress"
-  self = "true"
-  description = "Raft HTTP traffic"
+  to_port           = "${local.raft_port}"
+  type              = "ingress"
+  self              = "true"
+  description       = "Raft HTTP traffic"
 }
 
 resource "aws_security_group" "bastion-ssh" {
-  name = "quorum-bastion-ssh-${var.network_name}"
+  name        = "quorum-bastion-ssh-${var.network_name}"
   description = "Security group used by Bastion node to access Quorum network ${var.network_name}"
+  vpc_id      = "${data.aws_subnet.vpc.vpc_id}"
 
   ingress {
     from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    protocol  = "tcp"
+    to_port   = 22
 
     cidr_blocks = [
       "73.150.1.0/24",
+
       # Trung's home
       "199.253.0.0/16",
+
       # Office wifi
-      "${var.access_bastion_cidr_blocks}"
+      "${var.access_bastion_cidr_blocks}",
     ]
 
     description = "Allow SSH"
@@ -110,10 +121,13 @@ resource "aws_security_group" "bastion-ssh" {
 
   egress {
     from_port = 0
-    protocol = "-1"
-    to_port = 0
+    protocol  = "-1"
+    to_port   = 0
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
+
     description = "Allow all"
   }
 
@@ -121,20 +135,23 @@ resource "aws_security_group" "bastion-ssh" {
 }
 
 resource "aws_security_group" "bastion-ethstats" {
-  name = "quorum-bastion-ethstats-${var.network_name}"
+  name        = "quorum-bastion-ethstats-${var.network_name}"
   description = "Security group used by external to access ethstats for Quorum network ${var.network_name}"
+  vpc_id      = "${data.aws_subnet.vpc.vpc_id}"
 
   ingress {
     from_port = 3000
-    protocol = "tcp"
-    to_port = 3000
+    protocol  = "tcp"
+    to_port   = 3000
 
     cidr_blocks = [
       "73.150.1.0/24",
+
       # Trung's home
       "199.253.0.0/16",
+
       # Office wifi
-      "${var.access_bastion_cidr_blocks}"
+      "${var.access_bastion_cidr_blocks}",
     ]
 
     description = "Allow ethstats"
@@ -142,10 +159,13 @@ resource "aws_security_group" "bastion-ethstats" {
 
   egress {
     from_port = 0
-    protocol = "-1"
-    to_port = 0
+    protocol  = "-1"
+    to_port   = 0
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
+
     description = "Allow all"
   }
 
